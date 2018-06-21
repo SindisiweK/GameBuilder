faea0cf56da4d454b849b7213bd99e775fdd4f7e
21403037 <21403037@dut4life.ac.za>
2018-06-22 01:49:19 +0200

    begining of breakable toy budget manager

---
+++ ToyBuilderBudgetManager/ToyBuilderBudgetManager/BudgetManager.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using NUnit.Framework;
+using NUnit.Framework.Constraints;
+
+namespace ToyBuilderBudgetManager
+{
+    public class BudgetManager
+    {
+       
+        public List<BudgetModel> GenerateItemList()
+        {
+            var budgetIem = new List<BudgetModel>();
+
+                budgetIem.Add(new BudgetModel()
+                {
+                    expense = "EmergencyFund",
+                    cost = 2000,
+                    startDate = DateTime.Parse("2018-07-01"),
+                    endDate = DateTime.Parse("2018-12-31")
+                });
+                budgetIem.Add(new BudgetModel()
+                    { expense = "Savings",
+                      cost = 3500,
+                      startDate = DateTime.Parse("2018-07-01"),
+                      endDate = DateTime.Parse("2018-12-31") });
+                budgetIem.Add(new BudgetModel()
+                {
+                expense = "FoodAndGrocerie",
+                cost = 2100,
+                startDate = DateTime.Parse("2018-07-01"),
+                endDate = DateTime.Parse("2018-12-31")
+                });
+            return budgetIem;
+        }
+
+        private static string GetBudgetDuration(DateTime date)
+        {
+            DateTime beginningOfMonth = new DateTime(date.Year, date.Month, 1);
+
+            while (date.Date.AddDays(1).DayOfWeek != CultureInfo.CurrentCulture.DateTimeFormat.FirstDayOfWeek)
+
+                date = date.AddDays(1);
+
+            int weekNumber = (int)Math.Truncate((double)date.Subtract(beginningOfMonth).TotalDays / 7) + 1;
+
+            string[] weeks = { "first", "second", "third", "fourth", "fifth", "sixth" };
+
+            return weeks[weekNumber - 1];
+        }
+    }
+}
---
+++ ToyBuilderBudgetManager/ToyBuilderBudgetManager/BudgetManagerTest.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace ToyBuilderBudgetManager
+{
+    [TestFixture]
+    public class BudgetManagerTest
+    { 
+        [Test]
+        public void GenerateItemList_GivenOneItem_ShouldReturnItemAdded()
+        {
+            //Arrange
+            var sut = new BudgetManager();
+            var expected = "EmergencyFund";
+            //Act
+            var actual = sut.GenerateItemList();
+
+            //Assert
+            Assert.AreEqual(expected,actual.First().expense);
+        }
+        [Test]
+        public void GenerateItemList_GivenTwoItems_ShouldReturnItemAtTheIndex0f2()
+        {
+            //Arrange
+            var sut = new BudgetManager();
+            var expected = "Savings";
+            //Act
+            var actual = sut.GenerateItemList();
+
+            //Assert
+            Assert.AreEqual(expected,actual.ElementAt(1).expense);
+        }
+        [Test]
+        public void GenerateItemList_GivenThreeItems_ShouldReturnItemAtTheIndex0f3()
+        {
+            //Arrange
+            var sut = new BudgetManager();
+            var expected = "FoodAndGrocerie";
+            //Act
+            var actual = sut.GenerateItemList();
+
+            //Assert
+            Assert.AreEqual(expected,actual.ElementAt(2).expense);
+        }
+        //[Test]
+        //public void GenerateCost_GivenBasicSalary_ShouldReturnItemAtTheIndex0f3()
+        //{
+        //    //Arrange
+            
+        //    var sut = new BudgetManager();
+        //    var expected = "FoodAndGrocerie";
+        //    //Act
+        //    var actual = sut.GenerateItemList();
+
+        //    //Assert
+        //    Assert.AreEqual(actual.ElementAt(2), expected);
+        //}
+
+
+    }
+}
+
